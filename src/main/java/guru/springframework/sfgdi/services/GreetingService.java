78530d9b2ed3afe6e236437a6bb05e60b4afe841
Ciaran McKenna <hello@ciaranmckenna.com>
2022-05-16 14:18:55 +0100

    Project commit

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/26/19.
+ */
+@Service
+public interface GreetingService {
+
+    String sayGreeting();
+}
